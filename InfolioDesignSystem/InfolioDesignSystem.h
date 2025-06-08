f0cbb17f05bb4c6cf6fa79fa99bbed8a6f6e9e53
Volnei Foss <volneifoss@MacBook-Air-de-Volnei.local>
2025-06-07 22:35:04 -0300

    inital project

---
+++ InfolioDesignSystem/InfolioDesignSystem.h
@@ -0,0 +1,18 @@
+//
+//  InfolioDesignSystem.h
+//  InfolioDesignSystem
+//
+//  Created by Volnei Foss on 07/06/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InfolioDesignSystem.
+FOUNDATION_EXPORT double InfolioDesignSystemVersionNumber;
+
+//! Project version string for InfolioDesignSystem.
+FOUNDATION_EXPORT const unsigned char InfolioDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfolioDesignSystem/PublicHeader.h>
+
+
