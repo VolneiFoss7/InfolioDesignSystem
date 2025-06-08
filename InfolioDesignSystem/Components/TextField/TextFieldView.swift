227b1eb77c853a18e774f8410798e31d06e23c0e
Volnei Foss <volneifoss@MacBook-Air-de-Volnei.local>
2025-06-08 00:36:15 -0300

    including new files

---
+++ InfolioDesignSystem/Components/TextField/TextFieldView.swift
@@ -0,0 +1,108 @@
+//
+//  TextFieldView.swift
+//  InfolioDesignSystem
+//
+//  Created by Volnei Foss on 07/06/25.
+//
+
+import SwiftUI
+
+final class TextFieldViewModel: ObservableObject {
+    @Published var text: String = ""
+    @Published var searchActive: Bool = false
+}
+
+struct TextFieldView: View {
+    
+    enum Style {
+        case none
+        case outlined
+        
+        var borderColor: Color {
+            switch self {
+            case .none:
+                return .clear
+            case .outlined:
+                return .gray.opacity(0.2)
+            }
+        }
+        
+        var backgroundColor: Color {
+            switch self {
+            case .none:
+                return .clear
+            case .outlined:
+                return .gray.opacity(0.1)
+            }
+        }
+    }
+    
+    @ObservedObject var viewModel: TextFieldViewModel
+    var style: Style = .none
+    
+    var body: some View {
+        HStack {
+            if !viewModel.searchActive {
+                searchButton
+            } else {
+                searchInput
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .padding(16)
+    }
+    
+    var searchButton: some View {
+        Button(action: {self.viewModel.searchActive = true }) {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.gray)
+                .font(.system(size: 24))
+        }
+    }
+    
+    var searchInput: some View {
+        HStack {
+            TextField("Search", text: $viewModel.text)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(style.backgroundColor)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(style.borderColor, lineWidth: 1)
+                }
+                .overlay {
+                    if !self.viewModel.text.isEmpty {
+                        clearText
+                    }
+                }
+            
+            closeSearchButton
+
+        }
+    }
+    
+    private var closeSearchButton: some View {
+        Button(action: {
+            self.viewModel.searchActive = false
+        }) {
+            Text("Cancel")
+        }
+        .buttonStyle(.plain)
+
+    }
+    
+    private var clearText: some View {
+        Button(action: {
+            viewModel.text = ""
+        }) {
+            Image(systemName: "xmark.circle.fill")
+                .foregroundColor(.gray)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.horizontal, 32)
+        }
+    }
+}
+
+#Preview {
+    TextFieldView(viewModel: TextFieldViewModel(), style: .outlined)
+}
